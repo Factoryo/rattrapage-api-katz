937075f996c84d1b5a8e3df9a0e8ee576e3216bf
Rodolphe Katz <rodolphe.katz.k.g@gmail.com>
2023-09-28 22:01:54 +0200

    K

--- morpion-app/src/App.js
+++ morpion-app/src/App.js
@@ -1,44 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Login from './composants/Login';
-import SignUp from './composants/SignUp';
-import JoinJeu from './composants/JoinJeu';
-import React, { useState } from 'react';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isSignUpView, setIsSignUpView] = useState(false);
-  const [channel, setChannel] = useState(null);
-  const [userId, setUserId] = useState(null);
-
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-    setChannel(null);
-    setUserId(null);
-  }
-
-  const handleAuthentication = (id) => {
-    setIsAuthenticated(true);
-    setUserId(id);
-  }
-
   return (
-    <div>
-      {isAuthenticated ? (
-        <>
-        <JoinJeu channel={channel} setChannel={setChannel} userId={userId} />
-        <button onClick={handleLogout}>Déconnexion</button>
-        </>
-      ) : isSignUpView ? (
-        <>
-          <SignUp setIsAuth={handleAuthentication} />
-          <button onClick={() => setIsSignUpView(false)}>Aller à la connexion</button>
-        </>
-      ) : (
-        <>
-          <Login setIsAuth={handleAuthentication} />
-          <button onClick={() => setIsSignUpView(true)}>Créer un compte</button>
-        </>
-      )}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
